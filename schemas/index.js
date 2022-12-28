e800648c6bfea47649ecfe8cb5cbef7debc0be4e
jinhwan7 <rkflql851@gmail.com>
2022-12-28 21:03:57 +0900

    디비 연결수정

--- schemas/index.js
+++ schemas/index.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose');
 mongoose.set('strictQuery', true);
 const connect = () => {
-    mongoose.connect("mongodb://127.0.0.1:27017/node_beggin")
+    mongoose.connect("mongodb://test:sparta77@localhost:27017/admin", {
+        dbName: 'hanghae_week_3',
+        ignoreUndefined: true
+    })
         .catch(err => console.log(err));
 
     mongoose.connection.on("error", err => {
